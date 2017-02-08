2f31a43ac05427e64196d37ed851e412094b359f
Anu <nanuradhasarma@gmail.com>
2017-02-08 14:31:18 +0000

    Committing heapsort code

--- heap_sort.c
+++ heap_sort.c
@@ -1,83 +1,79 @@
 /*
+ *  * C Program to sort an array based on heap sort algorithm(MAX heap)
+ *   */ 
+#include <stdio.h>
+void max_heapify(int heap[],int no);
+void heap_sort(int heap[],int no);
+   
+void printarray(int A[], int n)
+{
+// function to print array
+ int i;
+ printf("\n");
+ for(i=0; i < n; i++)
+ 	printf("%d\t", A[i]);
+ printf("\n");
+}
 
- 	* C Program to sort an array based on  heap sort algorithm(MAX heap)
 
- 	*/
-
-	#include <stdio.h>
-	void heapsort(int [], int maxsize);
-	void buildmaxheap(int [], int);
-
-        void printarray(int A[10], int a, int b)
-	{
-         int i;
-   	 //printf("- calling A[10], %d %d\n", a, b);
-   	 for(i=a; i <= b; i++)
-   	 printf("%d ",A[i]);
-         printf("\n");
+void main()
+{
+    int heap[10], no, i;
+    printf("\n Enter no of elements :");
+    scanf("%d", &no);
+    printf("\n Enter the nos : ");
+    for (i = 0; i < no; i++)
+    scanf("%d", &heap[i]);
+    heap_sort(heap,no);
+    printarray(heap, no);
+    printf("\n");
+    return;
+}
+    void max_heapify(int heap[],int no)
+    {
+    int max,i,k,root,left,right,temp,temp2,pos;
+    max = no-1;
+    root=(max-1)/2;
+    while(root>=0)
+    {
+    left=root*2+1;
+    right=root*2+2;
+    if(right>max) {// single child case, if right node does not exist
+    	if(heap[left]>heap[root]) {//As right node does not exist, directly compare left and root nodes
+		temp=heap[left];
+		heap[left]=heap[root];
+		heap[root]=temp;
 	}
-
-	void heapsort(int A[10], int maxsize)
-	{
-    	int tmp;
-    	while (maxsize > 0) {
-    	buildmaxheap(A, maxsize);
-    	//swap A[0] with A[n-1];
-    	tmp = A[maxsize];
-    	A[maxsize] = A[0];
-    	A[0] = tmp;
-    	maxsize--;
+    }
+    else{
+    	if(heap[left]>heap[right]){
+    		temp=heap[left];
+    		pos=left;
+        }
+        else{
+    	     temp=heap[right];
+     	     pos=right;
     	}
-	}
+    	if(temp>heap[root]){
+    		temp2=heap[root];
+    		heap[root]=temp;
+    		heap[pos]=temp2;
+    	}
+    }
+    root--;
+    }//End of while loop
+    }
 
-
-	void buildmaxheap(int A[10], int n)
-	{
-    	int tmp,root, max, pos;
-    	while ( n > 0 ){
-	
-
-   	if ( n%2 == 0 ) { // even number, i.e. both left and right children exist
-   	root = (n-2) / 2 ; // right child root
-   	max = A[n] > A[n-1] ? A[n] : A[n-1];
-   	pos = (max == A[n] ) ? n : n-1;
-   	if( A[root] < max ) { //root has less value than childen max
-   	 tmp = A[root];
-   	 A[root] = max;
-   	 A[pos] = tmp;
-   	 } // if
-   	 n -= 2;
-   	 } // if
-
-   	 else { // odd number, only left child exist
-   	 root = (n-1)/2; // left child root;
-   	 if ( A[root] < A[n]) {//root has less value than left child
-   	 tmp = A[root];
-   	 A[root] = A[n];
-   	 A[n] = tmp;
-   	 } // if
-   	 n--; // decrement by 1
-   	 } // if
-
-
-    	 }
-	 }
-   	 
-
-
-
-	void main()
-	{
-   	 int heap[10], no, i;
-    	 printf("\n Enter no of elements :");
-    	 scanf("%d", &no);
-    	 printf("\n Enter the nos : ");
-    	 for (i = 0; i < no; i++)
-       	 scanf("%d", &heap[i]);
-   	 heapsort(heap, no-1);
-    	 printf("\nSorted array : ");
-    	 for (i=0; i < no; i++)
-      	 printf("%d ", heap[i]);
-    	 printf("\n");
-         return;
-	}
+void heap_sort(int heap[],int no)
+ {
+    int j,k,temp;
+    j = no-1;
+    while(j > 0)
+    {
+        max_heapify(heap,j+1);
+        temp = heap[0];
+        heap[0] = heap[j];//swap max ele with rightmost leaf ele 
+        heap[j] = temp;
+	j--;
+    }	
+ }
